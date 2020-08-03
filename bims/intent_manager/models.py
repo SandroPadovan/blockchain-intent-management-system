075bce13b7079c6ee4aa4dc5065af0d2472a88ae
SandroPadovan <sandropadovan@bluewin.ch>
2020-08-03 21:30:23 +0200

    added Intent model

--- bims/intent_manager/models.py
+++ bims/intent_manager/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from user_manager.models import User
 
-# Create your models here.
+
+class Intent(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    intent_string = models.TextField()
