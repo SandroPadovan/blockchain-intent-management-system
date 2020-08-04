3e6d522d68e61cc0b0474c795027c68f597b93c8
SandroPadovan <sandropadovan@bluewin.ch>
2020-08-04 21:34:18 +0200

    added methods which pass an intent to refiner when created

--- bims/intent_manager/api.py
+++ bims/intent_manager/api.py
@@ -1,6 +1,7 @@
 from intent_manager.models import Intent
 from rest_framework import viewsets, permissions
 from .serializers import IntentSerializer
+from refiner.refiner import refine_intent, save_policies
 
 
 class IntentViewSet(viewsets.ModelViewSet):
@@ -9,4 +10,13 @@ class IntentViewSet(viewsets.ModelViewSet):
         permissions.AllowAny
     ]
     serializer_class = IntentSerializer
-    queryset = Intent.objects.all()
+
+    def get_queryset(self):
+        """returns a queryset containing all intents"""
+        return Intent.objects.all()
+
+    def perform_create(self, serializer):
+        """Passes the intent to the refiner. Saves the intent if valid and saves the policies"""
+        policies = refine_intent(serializer)
+        intent_id = serializer.save().id
+        save_policies(policies, intent_id)
