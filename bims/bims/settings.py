276f8b1529bb8d2522d4375fc7529e43a8edba58
SandroPadovan <sandropadovan@bluewin.ch>
2020-08-08 16:22:41 +0200

    added knox in settings.py for token authentication

--- bims/bims/settings.py
+++ bims/bims/settings.py
@@ -38,11 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'knox',
     'user_manager',
     'intent_manager',
     'refiner',
     'policy_manager',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':
+        ('knox.auth.TokenAuthentication',)
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
