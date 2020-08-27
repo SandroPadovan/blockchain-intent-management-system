3e0505bf17a089f6727f6419bffea46ddc9e63d3
SandroPadovan <sandropadovan@bluewin.ch>
2020-08-27 15:21:00 +0200

    test: test_register_user added

--- bims/user_manager/tests.py
+++ bims/user_manager/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from user_manager.models import User
 
-# Create your tests here.
+
+class UserManagerTests(APITestCase):
+
+    url = '/api/auth/register'
+
+    def test_register_user(self):
+        """tests user creation and registration"""
+
+        data = {'username': 'testUser0',
+                'password': 'password'}
+
+        response = self.client.post(self.url, data, format='json')
+        print(response.data.get('token'))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'testUser0')
