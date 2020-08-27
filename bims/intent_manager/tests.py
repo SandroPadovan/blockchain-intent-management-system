b80dca9e09f9bfa8fd35eec0f4bce836d015f400
SandroPadovan <sandropadovan@bluewin.ch>
2020-08-27 15:21:28 +0200

    test: test_get_intents added

--- bims/intent_manager/tests.py
+++ bims/intent_manager/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
 
-# Create your tests here.
+
+class IntentManagerTests(APITestCase):
+    client = APIClient
+    url = '/api/intents/'
+    token = None
+
+    def setUp(self) -> None:
+        """obtain a valid token"""
+        credentials = {'username': 'testUser0',
+                       'password': 'password'}
+        self.token = self.client.post('/api/auth/register', credentials, format='json').data.get('token')
+
+    def test_get_intents_unauthorized(self):
+        """tests GET request without a token, expects a 401 status code"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_intents(self):
+        """tests GET request with token"""
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
