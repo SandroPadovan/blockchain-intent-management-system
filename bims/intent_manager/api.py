787b2651a33cf2be2c8e11cf8f0b4085d0e90879
SandroPadovan <sandropadovan@bluewin.ch>
2020-08-28 15:14:03 +0200

    Intent API: changed POST method and added PUT functionalities

--- bims/intent_manager/api.py
+++ bims/intent_manager/api.py
@@ -1,7 +1,11 @@
 from intent_manager.models import Intent
-from rest_framework import viewsets, permissions
 from .serializers import IntentSerializer
-from refiner.refiner import refine_intent, save_policies
+from refiner.refiner import refine_intent, save_policies, update_policies
+
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+
+from django.core.exceptions import PermissionDenied
 
 
 class IntentViewSet(viewsets.ModelViewSet):
@@ -15,8 +19,40 @@ class IntentViewSet(viewsets.ModelViewSet):
         """returns a queryset containing all intents"""
         return self.request.user.intents.all()
 
-    def perform_create(self, serializer):
+    def create(self, request, *args, **kwargs):
         """Passes the intent to the refiner. Saves the intent if valid and saves the policies"""
-        policies = refine_intent(serializer)
-        intent_id = serializer.save().id
-        save_policies(policies, intent_id)
+        policies = refine_intent(request.data.get('intent_string'))
+        intent = Intent(username=request.user,
+                        intent_string=request.data.get('intent_string'))
+        intent.save()
+        save_policies(policies, intent.id)
+
+        return Response({
+            'id': intent.id,
+            'created_at': intent.created_at,
+            'updated_at': intent.updated_at,
+            'intent_string': intent.intent_string,
+            'username': intent.username.id
+        }, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        """updates an intent"""
+        intent = Intent.objects.get(id=kwargs.get('pk'))
+
+        # if intent user and request user don't match
+        if request.user != intent.username:
+            raise PermissionDenied
+
+        policies = refine_intent(request.data.get('intent_string'))
+        intent.intent_string = request.data.get('intent_string')
+        intent.save()
+
+        update_policies(policies, intent.id)
+
+        return Response({
+            'id': intent.id,
+            'created_at': intent.created_at,
+            'updated_at': intent.updated_at,
+            'intent_string': intent.intent_string,
+            'username': intent.username.id
+        }, status=status.HTTP_200_OK)
