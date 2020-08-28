ba2e74ecfe3d97e1f2b400fd77702ab9ab5a9a74
SandroPadovan <sandropadovan@bluewin.ch>
2020-08-28 15:13:12 +0200

    added method update_policies

--- bims/refiner/refiner.py
+++ bims/refiner/refiner.py
@@ -2,13 +2,25 @@ from .irtk import refine
 from policy_manager.models import Policy
 
 
-def refine_intent(intent):
+def refine_intent(intent: str):
     """refines an intent and returns a list of resulting policies"""
-    policies = refine(intent.validated_data.get('intent_string'))
+    policies = refine(intent)
     return policies
 
 
-def save_policies(policies, intent_id):
-    """takes a list of policies and an intent_id as input, saves the policies with the intent_id"""
+def save_policies(policies, intent_id) -> None:
+    """takes a list of policies and an intent_id as input, creates and saves the policies with the intent_id"""
     for policy in policies:
         Policy.objects.create_policy(policy, intent_id)
+
+
+def update_policies(policies, intent_id) -> None:
+    """Updates existing policies by deleting the existing policies corresponding to an intent and saving the new
+    policies of the updated intent. """
+    # delete existing policies of given intent
+    old_policies = Policy.objects.filter(intent_id=intent_id)
+    for policy in old_policies:
+        policy.delete()
+
+    # create new policies
+    save_policies(policies, intent_id)
