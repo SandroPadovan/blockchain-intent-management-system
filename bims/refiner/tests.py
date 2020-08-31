1bce3aaffb2c11722a978f674ac2c84731d3c1ce
SandroPadovan <sandropadovan@bluewin.ch>
2020-08-31 23:19:27 +0200

    added testcases for update_policies

--- bims/refiner/tests.py
+++ bims/refiner/tests.py
@@ -80,3 +80,73 @@ class RefinerTests(TestCase):
         self.assertEqual(Policy.objects.count(), 1, 'More than one policy was saved.')
         self.assertEqual(Policy.objects.get().threshold, 24, 'Threshold incorrect')
         self.assertIsNotNone(Policy.objects.get().id, 'Policy id was None')
+
+    def test_update_single_policy(self):
+        policy = Policy(intent_id=self.intent, currency=Currency.objects.get(currency='USD'))
+        policy.save()
+
+        irtk_policy = irtk_policy_factory('client2', 10)
+        policy_list = [irtk_policy]
+
+        update_policies(policy_list, self.intent.id)
+
+        self.assertEqual(len(Policy.objects.all()), 1, 'Incorrect number of policies in database')
+        self.assertEqual(Policy.objects.get().threshold, 10, 'Incorrect threshold of policy')
+        self.assertEqual(Policy.objects.get().user, 'client2', 'Incorrect policy user')
+
+    def test_update_policies_with_deleting_policies(self):
+        policy1 = Policy(intent_id=self.intent, currency=Currency.objects.get(currency='USD'))
+        policy1.save()
+        policy2 = Policy(intent_id=self.intent, currency=Currency.objects.get(currency='USD'))
+        policy2.save()
+
+        irtk_policy = irtk_policy_factory('client3', 22)
+        policy_list = [irtk_policy]
+
+        update_policies(policy_list, self.intent.id)
+
+        self.assertEqual(len(Policy.objects.all()), 1, 'Incorrect number of policies in database')
+        self.assertEqual(Policy.objects.get().threshold, 22, 'Incorrect threshold of policy')
+        self.assertEqual(Policy.objects.get().user, 'client3', 'Incorrect policy user')
+
+    def test_update_policies_with_adding_policies(self):
+        policy = Policy(intent_id=self.intent, currency=Currency.objects.get(currency='USD'))
+        policy.save()
+
+        irtk_policy1 = irtk_policy_factory('client1', 19)
+        irtk_policy2 = irtk_policy_factory('client2', 12)
+        policy_list = [irtk_policy1, irtk_policy2]
+
+        update_policies(policy_list, self.intent.id)
+
+        self.assertEqual(len(Policy.objects.all()), 2, 'Incorrect number of policies in database')
+        self.assertEqual(Policy.objects.filter(user='client1')[0].threshold, 19, 'Incorrect threshold of policy')
+        self.assertEqual(Policy.objects.filter(user='client2')[0].threshold, 12, 'Incorrect threshold of policy')
+        self.assertTrue(bool(Policy.objects.all()[0].created_at != Policy.objects.all()[0].updated_at)
+                        != bool(Policy.objects.all()[1].created_at != Policy.objects.all()[1].updated_at))
+
+
+def irtk_policy_factory(user: str, threshold: int) -> irtkPolicy:
+    """helper function which returns an instance of irtkPolicy which can be used in tests"""
+    irtk_policy = irtkPolicy(
+        user=user,
+        cost_profile=CostProfile.PERFORMANCE,
+        timeframe_start=Time.DEFAULT,
+        timeframe_end=Time.DEFAULT,
+        interval=Interval.DAILY,
+        currency=irtkCurrency.USD,
+        threshold=threshold,
+        split_txs=False,
+        blockchain_pool=set(),
+        blockchain_type=BlockchainType.INDIFFERENT,
+        min_tx_rate=4,
+        max_block_time=600,
+        min_data_size=20,
+        max_tx_cost=0.0,
+        min_popularity=0.0,
+        min_stability=0.0,
+        turing_complete=False,
+        encryption=False,
+        redundancy=False
+    )
+    return irtk_policy
