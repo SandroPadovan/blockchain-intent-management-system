8ef520e68e096509bc49a285457006283755af29
SandroPadovan <sandropadovan@bluewin.ch>
2020-09-01 16:38:40 +0200

    changed policy API to return unpickled blockchain pool

--- bims/policy_manager/api.py
+++ bims/policy_manager/api.py
@@ -1,8 +1,10 @@
 from .models import Policy
 from intent_manager.models import Intent
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
 from .serializers import PolicySerializer
 from django.core.exceptions import PermissionDenied
+import pickle
 
 
 class PolicyViewSet(viewsets.ReadOnlyModelViewSet):
@@ -19,11 +21,21 @@ class PolicyViewSet(viewsets.ReadOnlyModelViewSet):
         return intent.username == user
 
     def get_queryset(self):
-        """returns policies corresponding to an intent specified in the query parameters,
+        """returns a queryset of policies corresponding to an intent specified in the query parameters,
         if the user is the owner of the intent"""
         intent_id = self.request.query_params.get('intent_id', None)
         if self.is_intent_owner(self.request.user, intent_id):
             queryset = Policy.objects.filter(intent_id_id=intent_id)
             return queryset
-
         raise PermissionDenied
+
+    def list(self, request, *args, **kwargs):
+        """Handles the GET request for policies. Unpickles the blockchain_pool of each policy"""
+        queryset = self.get_queryset().values()
+        for policy in queryset:
+            policy['blockchain_pool'] = pickle.loads(policy['blockchain_pool'])
+            blockchain_pool = []
+            for blockchain in policy['blockchain_pool']:
+                blockchain_pool.append(blockchain.name)
+            policy['blockchain_pool'] = blockchain_pool
+        return Response(queryset, content_type='application/json')
