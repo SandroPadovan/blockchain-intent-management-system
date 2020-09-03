0ba82394bb057e3d45735b785ffe3307f24bc884
SandroPadovan <sandropadovan@bluewin.ch>
2020-09-03 14:41:19 +0200

    included react

--- bims/bims/urls.py
+++ bims/bims/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('frontend.urls')),
     path('', include('intent_manager.urls')),
     path('', include('policy_manager.urls')),
     path('', include('user_manager.urls')),
