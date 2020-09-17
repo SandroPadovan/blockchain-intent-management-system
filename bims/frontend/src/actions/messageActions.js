e15e48b358f16d166fe0cdf2e6437c96e292ca98
SandroPadovan <sandropadovan@bluewin.ch>
2020-09-17 16:23:42 +0200

    added Message Reducer

---
+++ bims/frontend/src/actions/messageActions.js
@@ -0,0 +1,17 @@
+import {CREATE_MESSAGE, GET_ERRORS, GET_MESSAGES} from "./types";
+
+// Create Message
+export const createMessage = msg => {
+    return {
+        type: CREATE_MESSAGE,
+        payload: msg
+    }
+}
+
+// return error
+export const returnErrors = (msg, status) => {
+    return {
+        type: GET_ERRORS,
+        payload: { msg, status }
+    }
+}
