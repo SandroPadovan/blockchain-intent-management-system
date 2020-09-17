5c76f6d5ee3366284ea7dd6825b2b08c560515a1
SandroPadovan <sandropadovan@bluewin.ch>
2020-09-17 22:07:35 +0200

    added all Routes and Components

--- bims/frontend/src/components/App.js
+++ bims/frontend/src/components/App.js
@@ -1,14 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-
-import Intents from "./intents/Intents";
-import Login from "./accounts/Login";
-
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
+import Alerts from "./layout/Alerts";
+import Home from "./common/Home";
+import Help from "./common/Help";
+import IntentOverview from "./intents/IntentOverview";
+import IntentCreation from "./intents/IntentCreation";
+import IntentDetails from "./intents/IntentDetails";
+import IntentEdit from "./intents/IntentEdit";
+import Login from "./accounts/Login";
+import Register from "./accounts/Register";
+import Header from "./layout/Header";
+import PrivateRoute from "./common/PrivateRoute";
+
 import store from "../store";
 import { loadUser } from "../actions/auth";
 
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+
+// Alert options
+const alertOptions = {
+    timeout: 5000,
+    position: 'top center',
+};
 
 class App extends Component {
 
@@ -19,12 +36,26 @@ class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <Router>
-                    <h1>Blockchain Intent Management System</h1>
-                    <Switch>
-                        <Route exact path="/login" component={Login} />
-                    </Switch>
-                </Router>
+                <AlertProvider template={AlertTemplate} {...alertOptions}>
+                    <Router>
+                        <Fragment>
+                            <Header/>
+                            <Alerts/>
+                            <div className="container">
+                                <Switch>
+                                    <Route exact path="/" component={Home} />
+                                    <Route exact path="/login" component={Login} />
+                                    <Route exact path="/register" component={Register}/>
+                                    <Route exact path="/help" component={Help}/>
+                                    <PrivateRoute exact path="/overview" component={IntentOverview} />
+                                    <PrivateRoute exact path="/create-intent" component={IntentCreation}/>
+                                    <PrivateRoute exact path="/intent/:id" component={IntentDetails}/>
+                                    <PrivateRoute exact path="/intent/:id/edit" component={IntentEdit}/>
+                                </Switch>
+                            </div>
+                        </Fragment>
+                    </Router>
+                </AlertProvider>
             </Provider>
         );
     }
