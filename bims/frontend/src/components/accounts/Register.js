1f3e55dba7f48cf91cffd02c978ef43c51d18a36
SandroPadovan <sandropadovan@bluewin.ch>
2020-09-17 19:28:06 +0200

    added password confirmation in registration

--- bims/frontend/src/components/accounts/Register.js
+++ bims/frontend/src/components/accounts/Register.js
@@ -3,21 +3,29 @@ import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { register } from "../../actions/auth";
+import { createMessage } from "../../actions/messageActions";
 
 class Register extends Component {
     state = {
         username: "",
-        password: ""
+        password: "",
+        passwordConfirmation: "",
     }
 
     static propTypes = {
         register: PropTypes.func.isRequired,
+        createMessage: PropTypes.func.isRequired,
         isAuthenticated: PropTypes.bool
     }
 
     onSubmit = e => {
         e.preventDefault();
-        this.props.register(this.state.username, this.state.password);
+        const { password, passwordConfirmation } = this.state;
+        if (password !== passwordConfirmation) {
+            this.props.createMessage({ passwordNotMatch: 'Passwords do not match' });
+        } else {
+            this.props.register(this.state.username, this.state.password);
+        }
     }
 
     onChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -26,7 +34,7 @@ class Register extends Component {
         if (this.props.isAuthenticated) {
             return <Redirect to="/overview"/>
         }
-        const {username, password} = this.state;
+        const {username, password, passwordConfirmation} = this.state;
         return (
             <div>
                 <div className="col-md-6 m-auto">
@@ -53,6 +61,16 @@ class Register extends Component {
                                     onChange={this.onChange}
                                 />
                             </div>
+                            <div className="form-group">
+                                <label>Confirm Password</label>
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    name="passwordConfirmation"
+                                    value={passwordConfirmation}
+                                    onChange={this.onChange}
+                                />
+                            </div>
                             <div>
                                 <button
                                     type="submit"
@@ -71,4 +89,4 @@ const mapStateToProps = state => ({
     isAuthenticated: state.auth.isAuthenticated
 })
 
-export default connect(mapStateToProps, { register })(Register);
+export default connect(mapStateToProps, { register, createMessage })(Register);
