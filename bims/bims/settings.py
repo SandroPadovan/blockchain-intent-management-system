df49c4ea0c026ea14aae62a6bc924989c17b05e6
SandroPadovan <sandropadovan@bluewin.ch>
2020-10-21 14:37:03 +0200

    changed database from sqlite to postgres

--- bims/bims/settings.py
+++ bims/bims/settings.py
@@ -84,10 +84,16 @@ WSGI_APPLICATION = 'bims.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bims',
+        'USER': 'bims_admin',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+
 
 AUTH_USER_MODEL = 'user_manager.User'
 
